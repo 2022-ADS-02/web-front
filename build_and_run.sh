ec5492c054eea81b561a37bf966d9adba7c8a350
ggyul <wakegyul@gmail.com>
2022-11-21 00:42:21 +0900

    chore: 도커 빌드 후 실행하는 쉘 스크립트 추가한다

---
+++ build_and_run.sh
@@ -0,0 +1,10 @@
+image_name='gorae-front'
+container_name='gorae-front-container'
+port=8000
+
+docker stop $container_name
+docker rm $container_name
+docker image rm $image_name
+
+docker build --no-cache -t $image_name .
+docker run -it -d --name $container_name -p $port:$port $image_name
