631bde95fc94442e6a3a0c125080620c5ed40130
ggyul <wakegyul@gmail.com>
2022-11-20 17:12:16 +0900

    feat: 서버 응답 없을 시에 에러만 콘솔에 출력한다

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import requests
-from bs4 import BeautifulSoup
 import uvicorn
 from fastapi import FastAPI, Request, Depends, Form, status
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from httpx import AsyncClient
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -14,6 +13,12 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 problem_info = {}
 
 cur_problem = ''
+
+
+@app.get('/favicon.ico', include_in_schema=False)
+async def favicon():
+    return FileResponse("static/image/kuwhale.png")
+
 
 @app.get("/")
 async def home(request: Request):
@@ -31,21 +36,28 @@ async def home(request: Request):
                                                          "samples_text"] if "samples_text" in problem_info else "",
                                                      })
 
+
 # 문제 번호 제출 시
 @app.post("/search")
 async def search_boj_problem(request: Request, task: str = Form(...)):
     # 크롤링 서버로부터 받아옴
     global cur_problem
     cur_problem = task
+    # async with AsyncClient(base_url="http://172.17.0.1:7000/search/") as ac:
     async with AsyncClient(base_url="http://localhost:7000/search/") as ac:
-        response = await ac.get(task)
-        response_json = response.json()
-        problem_info["problem_description"] = response_json["problem_description"]
-        problem_info["problem_input"] = response_json["problem_input"]
-        problem_info["problem_output"] = response_json["problem_output"]
-        problem_info["samples"] = response_json["samples"]
-        problem_info["samples_text"] = response_json["samples_text"]
+        try:
+            response = await ac.get(task)
+            response_json = response.json()
+            problem_info["problem_description"] = response_json["problem_description"]
+            problem_info["problem_input"] = response_json["problem_input"]
+            problem_info["problem_output"] = response_json["problem_output"]
+            problem_info["samples"] = response_json["samples"]
+            problem_info["samples_text"] = response_json["samples_text"]
+        except Exception as e:
+            print(e)
+
         return RedirectResponse(url=app.url_path_for("home"), status_code=status.HTTP_303_SEE_OTHER)
+
 
 @app.post("/scoring")
 async def send_request_to_scoring(request: Request, batch_content: str = Form(...)):
@@ -55,6 +67,7 @@ async def send_request_to_scoring(request: Request, batch_content: str = Form(..
         print(problem_info["samples_text"])
     # {"code": batch_content}
     return RedirectResponse(url=app.url_path_for("home"), status_code=status.HTTP_303_SEE_OTHER)
+
 
 if __name__ == '__main__':
     # get_problem_info(1000)
